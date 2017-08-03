d2af5360a2c660af7d785cb7461c628f04c3a312
AnthonyGillan <ag14125@my.bristol.ac.uk>
2017-08-03 18:43:06 +0100

    tidying up, getting rid of unused lines

--- create_sentence.py
+++ create_sentence.py
@@ -92,7 +92,6 @@ def s_concatenate(sentence, word_length, freq, sentence_number):
 
     s = []
     silence_lenght = 0.2 - (0.2*word_length)			# shorter the longer the words are stretched to
-    # maybe add in a line that extends the length of silence after the middle word
     if silence_lenght<0:
     	silence_lenght=0
 
@@ -120,17 +119,17 @@ def stretch_simple(sound, f, window_size=2**10, h=2**10/4):
 
     for i in np.arange(0, int(len(sound)) - int((window_size + h)), int(h*f)):
 
-	               # two potentially overlapping subarrays
+	    # two potentially overlapping subarrays
 	    a1 = sound[i: i + window_size]
 	    a2 = sound[i + h: i + window_size + h]
 
-	                # resynchronize the second array on the first
+	    # resynchronize the second array on the first
 	    s1 =  np.fft.fft(hanning_window * a1)
 	    s2 =  np.fft.fft(hanning_window * a2)
 	    phase = (phase + np.angle(s2/s1)) % 2*np.pi
 	    a2_rephased = np.fft.ifft(np.abs(s2)*np.exp(1j*phase))
 
-	                # add to result
+	    # add to result
 	    i2 = int(i/f)
 	    result[i2 : i2 + window_size] += np.real(hanning_window * a2_rephased)
 
@@ -170,67 +169,6 @@ def fades(word, percentage_fade):
 
 	return word
 
-def peak_envelope(sound, peak_separation, min_amplitude):
-	# envelope=np.zeros(sound.shape)
-	# indexes=[]
-	# peaks=[]
-	# indexes=peakutils.indexes(sound, thres=10, min_dist=20)
-	# for k in range(0, len(indexes)):
-	#  	peaks.append(sound[indexes[k]])
-
-	# interpolation_function = interp1d(indexes,peaks, kind='cubic', bounds_error=False, fill_value=0.0)
-
-	# # Evaluate each model over the domain of (s)
-	# for k in xrange(0,len(sound)):
-	#     envelope[k]=interpolation_function(k)
-
-	# return envelope
-
-	filtered_wav=sound
-	envelope=np.zeros(filtered_wav.shape)
-
-	# Prepend the first value of (s) to the interpolating values. 
-	# This forces the model to use the same starting point for both the upper and lower envelope models.
-	# Lists with the same starting point
-
-	peak_x = [0,]
-	peak_y = [filtered_wav[0],]
-	spaced_peak_x = []
-	spaced_peak_y = []
-
-	# Detect peaks and mark their location in peak_x, peak_y
-	# xrange better for large range 
-
-	for k in xrange(0,len(filtered_wav) - 1):
-	    if (sign(long(filtered_wav[k]) - long(filtered_wav[k-1])) == 1 
-	    	and sign(long(filtered_wav[k]) - long(filtered_wav[k+1])) == 1):	# get peak
-			peak_x.append(k)
-			peak_y.append(filtered_wav[k])
-		        if (k>2 and (peak_x[-1] - peak_x[-2]) > peak_separation):		# if peaks arre separated by
-		        	if (filtered_wav[k] > min_amplitude):						# if the peaks are above a min
-			        	spaced_peak_x.append(k)
-			        	spaced_peak_y.append(filtered_wav[k])
-
-	# Append the last value of (wav_sentece) to the interpolating values. 
-	# This forces the model to use the same ending point for the envelope model.
-	# [-1] is the last element of a list
-
-	peak_x.append(len(filtered_wav) - 1) 	# last value of index of wav_sentence
-	peak_y.append(filtered_wav[-1])		# last value of wav_sentence
-
-	# Fit suitable model to the data.
-	# interp1d creates a 1D interpolation function to find value of new points
-
-	interpolation_function = interp1d(spaced_peak_x,spaced_peak_y, kind='slinear', bounds_error=False, fill_value=0.0)
-
-	# Evaluate each model over the domain of filtered_wav
-	for k in xrange(0, len(filtered_wav)):
-	    envelope[k] = interpolation_function(k)
-
-	rms_envelope = window_rms(envelope, window_size=100)
-	rms_envelope = window_rms(rms_envelope, window_size=200)
-	return rms_envelope
-
 def window_rms(signal, window_size):
 	a2 = np.power(signal,2)
 	window = np.ones(window_size)/float(window_size)
@@ -256,62 +194,47 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order):
 
 def main(argv):
 
-	base_l = 0.4                                            # 150ms base length
+	base_l = 0.4                                            # base length
 	new_l_av = 0.2                                          # new average length 
-	k = 0.1                                                  # shape of distribution
+	k = 0.1                                                 # shape of distribution
 	theta = new_l_av/k                                      # scale of distribution
 
 	sentence_file = ''
 	new_length = 1.0
 
-	num_gammatone_bands = 32
-	gammatone_cutoff_l = 50
-
-	x = np.linspace (0, 100, 200) 
-	y1 = stats.gamma.pdf(x, a=80, scale=0.1)
-	plt.plot(x, y1, "y-", label=(r'$\alpha=29, \beta=3$')) 
-
-
-	plt.ylim([0,0.08])
-	plt.xlim([0,150])
-	plt.show()
-
 	try:                                                   # runs in entirety unless exception
-		opts, args = getopt.getopt(argv,"hb:e:s:",["sfile=","stretch="])   # Parses command line opts and param list. (args, shortopts, longopts=[]), short -h,-i: colon if requires argument
+		opts, args = getopt.getopt(argv,"hb:e:n:", ["sfile=", "stretch=", "newl=" ])
 	except getopt.GetoptError:                             # raised when an unrecognized option is found in the argument list or when an option requiring an argument is given none
 		print 'load_sentence.py -i <sentence_file> -s <new_length(float)>'         # <input file>
 		sys.exit(2)                                         # exit from python -> arg is an int (2) giving the exit status
 	for opt, arg in opts:                                  # cycle through options
 		if opt == '-h':
-			print 'load_sentence.py -i <sentence_file -s <new_length(float)>'     # <input file>
-			sys.exit()                                      # exit from python
-		elif opt in ("-b", "--begin"):                      # if option is -i ie// input
-			start_file = float(arg)                              # sentence file = arguments
+			print 'load_sentence.py -b <start sentence number> -e <end sentence number> -n <new word length (s) for words other than middle>'
+			sys.exit()                                    # exit from python
+		elif opt in ("-b", "--begin"):                    # the sentence file to begin from
+			start_file = float(arg)                       
 			start_file = int(start_file)         
-		elif opt in ("-e", "--end"):                      # if option is -i ie// input
-			stop_file = float(arg)                              # sentence file = arguments
+		elif opt in ("-e", "--end"):                      # the sentence file to end at
+			stop_file = float(arg)                        
 			stop_file = int(stop_file)
-		elif opt in ("-s", "--stretch"):                      # if option is -i ie// input
-			new_length = float(arg)                       # stretch facotr = arguments
+		elif opt in ("-n", "--newl"):                  	  # the length in seconds to stretch to
+			new_length = float(arg)                       
 			print 'new length is', new_length
 
 	for sentence_file in range(start_file, stop_file+1):
-		s = Sentence(str(sentence_file), new_length)         # create a Sentence object s
-
-		# create a gammatone filterbank
-		coefs = gammatone_create(samp_freq=s.freq, num_freqs=num_gammatone_bands, lower_cutoff=gammatone_cutoff_l)
+		s = Sentence(str(sentence_file), new_length)       	# create a Sentence object s
 
 		control_words = []
 
 	   	for i in range(s.length):
-	   		if s.new_length !=0:				# new_length of 0 means no stretch
-		   		if i != s.length/2: 		 	# don't stretch the middle word
+	   		if s.new_length !=0:							# new_length of 0 means no stretch
+		   		if i != s.length/2: 		 				# don't stretch the middle word
 			   		new_l = s.new_length
-			   		new_l = base_l + gammavariate(k, theta)
+			   		# new_l = base_l + gammavariate(k, theta)
 			   		print 'new_l', new_l
 			   		old_l = s.t_lengths[i]
 			  		print 'old_l', old_l
-			  		# stretch_f=old_l/new_l # eg stretch factor 0.5 -> 0.5 times original
+			  		# stretch_f=old_l/new_l 				# use with stretch_simple
 			   		stretch_f = new_l/old_l
 			   		# s.words[i]=stretch_simple(s.words[i], stretch_f)
 			   		s.words[i] = stretch(s.words[i], stretch_f)
@@ -323,16 +246,13 @@ def main(argv):
 			   		s.words[i] = stretch(s.words[i], stretch_f)
 			   		s.words[i] = fades(s.words[i], percentage_fade=0.1)
 			   		control_words.append(s.words[i])
-			else:							# new_length of 0 means no stretch
+			else:											# new_length of 0 means no stretch
 				# stretch_f=1
-		   		# s.words[i] = stretch(s.words[i], stretch_f) # optional to put through phase vocoder without stretch 
+		   		# s.words[i] = stretch(s.words[i], stretch_f)
 		   		control_words.append(s.words[i])
 
-
-		control_s = Control_sentence(control_words, s.freq, s.word_names) 	# feed data into class
-	   	wav_sentence = s_concatenate(control_s, s.new_length, s.freq, str(sentence_file))								# use the class to create a .wav file of the sentence 
-
-	   	samples_in_manip_sentence = len(wav_sentence)
+		control_s = Control_sentence(control_words, s.freq, s.word_names) 					# feed data into class
+	   	wav_sentence = s_concatenate(control_s, s.new_length, s.freq, str(sentence_file))	# use the class to create a .wav file of the sentence 
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
